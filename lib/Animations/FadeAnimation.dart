e70788a51a0ce3bb1be905e64aa8aa4f54edf49c
mohaanselmeIO <ouattara902@gmail.com>
2020-05-19 01:20:21 +0000

    premier github project

--- lib/Animations/FadeAnimation.dart
+++ lib/Animations/FadeAnimation.dart
@@ -6,7 +6,7 @@ class FadeAnimation extends StatelessWidget {
   final Widget child;
 
   FadeAnimation(this.delay, this.child);
-
+//foolish
   @override
   Widget build(BuildContext context) {
     final tween = MultiTrackTween([
